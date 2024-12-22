1c625998ed4c5867f3f707ad98c7715e341f0543
Algorhythmics <intivioli@gmail.com>
2024-12-22 15:20:03 +0100

    Update

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
-fn main() {
+use dotenv::dotenv;
 
+fn main() {
+    dotenv().ok();
+
+    let profile = std::env::var("PROFILE").unwrap_or_else(|_| "unknown".to_string());
+
+    let db_url;
+    let db
 }
