70478704913e2764ba2406295d4211e30756d096
Algorhythmics <intivioli@gmail.com>
2024-12-26 15:25:23 +0100

    Update

--- build.rs
+++ build.rs
@@ -6,20 +6,12 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_else(|_| "unknown".to_string());
 
     let db_url: String;
-    let db_user: String;
-    let db_password: String;
 
     if profile == "release" {
         db_url = std::env::var("PROD_DB_URL").expect("Failed to read production DB_URL.");
-        db_user = std::env::var("PROD_DB_USER").expect("Failed to read production DB_USER.");
-        db_password = std::env::var("PROD_DB_USER").expect("Failed to read production DB_PASSWORD.");
     } else {
         db_url = std::env::var("TEST_DB_URL").expect("Failed to read testing DB_URL.");
-        db_user = std::env::var("TEST_DB_USER").expect("Failed to read testing DB_USER.");
-        db_password = std::env::var("TEST_DB_USER").expect("Failed to read testing DB_PASSWORD.");
     }
 
     println!("cargo:rustc-env=DB_URL={}", db_url);
-    println!("cargo:rustc-env=DB_USER={}", db_user);
-    println!("cargo:rustc-env=DB_PASSWORD={}", db_password);
 }
--- src/api/control/sql.rs
+++ src/api/control/sql.rs
@@ -1,5 +1,27 @@
-use crate::api::typedef::user::User;
+use std::sync::{LazyLock, Mutex};
+
+use crate::api::typedef::User;
+use mysql::{Opts, Pool};
+use mysql::prelude::*;
+
+const DB_URL: &str = env!("DB_URL");
+
+const POOL: LazyLock<Mutex<Pool>> = LazyLock::new(|| Mutex::new(Pool::new(Opts::from_url(DB_URL).expect("Error creating opts.")).expect("Error initializing database pool.")));
+
+pub fn init_db() -> Result<(), Box<dyn std::error::Error>> {
+    let schema = include_bytes!("../../../schema.sql");
+    let queries: Vec<&str> = String::from_utf8_lossy(schema).split(';').collect();
+    
+    Ok(())
+}
+
+pub fn query_unsafe(str: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = POOL.get_conn()?;
+
+    conn.query_drop(str)?;
+    Ok(())
+}
 
 pub async fn get_player(uuid: String) -> User {
 
-}
+} 
