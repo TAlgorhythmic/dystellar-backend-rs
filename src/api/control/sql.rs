7f4759dc1384426851e4302e4eee1f191fa88388
Algorhythmics <intivioli@gmail.com>
2024-12-26 16:45:43 +0100

    Update

--- src/api/control/sql.rs
+++ src/api/control/sql.rs
@@ -6,7 +6,18 @@ use mysql::prelude::*;
 
 const DB_URL: &str = env!("DB_URL");
 
-const POOL: LazyLock<Mutex<Pool>> = LazyLock::new(|| Mutex::new(Pool::new(Opts::from_url(DB_URL).expect("Error creating opts.")).expect("Error initializing database pool.")));
+const POOL: LazyLock<Mutex<Option<Pool>>> = LazyLock::new(|| Mutex::new(None));
+
+fn get_pool() -> Pool {
+    let mut pool = POOL.lock().unwrap();
+
+    if pool.is_none() {
+        let new_pool = Pool::new(Opts::from_url(DB_URL).expect("Error creating opts.")).expect("Error initializing database pool.");
+
+        *pool = Some(new_pool);
+    };
+    pool.clone().unwrap()
+}
 
 pub fn init_db() -> Result<(), Box<dyn std::error::Error>> {
     let schema = include_bytes!("../../../schema.sql");
@@ -16,8 +27,7 @@ pub fn init_db() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 pub fn query_unsafe(str: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut conn = POOL.get_conn()?;
-
+    
     conn.query_drop(str)?;
     Ok(())
 }
