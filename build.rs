201255e417ff51a8ab95704927aec522d22c2f7c
Algorhythmics <intivioli@gmail.com>
2025-01-19 00:30:50 +0100

    Update

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use std::env::var;
 
 fn main() {
     dotenv().ok();
@@ -6,12 +7,21 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_else(|_| "unknown".to_string());
 
     let db_url: String;
+    let host: String;
+    let port: i32;
 
     if profile == "release" {
-        db_url = std::env::var("PROD_DB_URL").expect("Failed to read production DB_URL.");
+        db_url = var("PROD_DB_URL").expect("Failed to read production DB_URL.");
+        host = var("PROD_HOST").expect("Failed to read production HOST.");
+        port = i32::from_str_radix(var("PROD_PORT").expect("Failed to read production PORT.").as_str(), 10).expect("Failed to parse int.");
     } else {
         db_url = std::env::var("TEST_DB_URL").expect("Failed to read testing DB_URL.");
+        host = var("TEST_HOST").expect("Failed to read production HOST.");
+        port = i32::from_str_radix(var("TEST_PORT").expect("Failed to read production PORT.").as_str(), 10).expect("Failed to parse int.");
+
     }
 
     println!("cargo:rustc-env=DB_URL={}", db_url);
+    println!("cargo:rustc-env=HOST={}", host);
+    println!("cargo:rustc-env=PORT={}", port);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 mod api;
 use api::control::sql::setup::init_db;
+use std::net::{SocketAddr, IpAddr};
+use tokio::net::TcpListener;
+use hyper_util::rt::TokioIo;
 
+const HOST: &str = env!("HOST");
+const PORT: &str = env!("PORT");
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // Init Database
     init_db().await.expect("Failed to initialize database");
+    
+    let address = SocketAddr::new(HOST.parse::<IpAddr>().expect("Failed to parse host."), u16::from_str_radix(PORT, 10).expect("Failed to parse port."));
+    let binding = TcpListener::bind(address).await?;
 
+    loop {
+        let (stream, addr) = binding.accept().await?;
+
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+
+        })
+    }
 }
