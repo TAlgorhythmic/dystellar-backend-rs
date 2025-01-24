48c1dd7777abca0bef74b6a41b2fab9b6982ad90
Algorhythmics <intivioli@gmail.com>
2025-01-24 14:38:10 +0100

    Update

--- src/api/typedef/routing.rs
+++ src/api/typedef/routing.rs
@@ -11,35 +11,53 @@ pub enum Method {
     Patch,
 }
 
+impl From<&str> for Method {
+    fn from(value: &str) -> Self {
+        if value == "POST" {return Self::Post;}
+        else if value == "DELETE" {return Self::Delete;}
+        else if value == "PATCH" {return Self::Patch;}
+        else {Self::Get}
+    }
+}
+
 pub struct Endpoint {
     param: Option<Box<str>>,
     method: Method,
     name: Box<str>,
-    run: fn() -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>>,
+    run: fn(Option<Box<str>>) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>>,
 }
 
 pub struct Node {
+    name: Box<str>,
     subnodes: Vec<Node>,
     endpoints: Vec<Endpoint>,
 }
 
 pub struct Router {
-    base: Box<str>,
-    nodes: Vec<Node>,
+    base: Node,
 }
 
 impl Router {
     pub fn new(base: &str) -> Self {
-        Self { base: base.into(), nodes: vec![] }
+        Self { base: base.into()} }
     }
 
-    pub fn endpoint(&self, method: Method, path: &str) {
-        let mut split = path.split('/');
+    pub fn endpoint<T>(&self, method: Method, path: &str, func: T) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+    where
+        T: Fn(Option<Box<str>>) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>>
+    {
+        let split = path.split('/').collect::<Vec<&str>>();
+        let len = split.len();
 
-        let mut actual = split.next();
-        while actual.is_some() && actual.unwrap().eq(self.base.deref()) {
-            actual = split.next();
+        if len < 2 {
+            Err("An endpoint must contain at least the basename.")
         }
-        
+
+        for i in 2..len {
+            if i == (len - 1) {
+
+            }
+        }
+        Ok(())
     }
 }
