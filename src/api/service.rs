6725f90979efe2afcd6e939110b06ddc835cdd18
Algorhythmics <intivioli@gmail.com>
2025-08-01 19:03:47 +0200

    Add nice colors to messages

--- src/api/service.rs
+++ src/api/service.rs
@@ -1,16 +1,17 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::sync::Arc;
 
 use hyper::body::{Incoming, Bytes};
 use hyper::{Request, Response};
 use http_body_util::Full;
 use json::object;
-use tokio::sync::Mutex;
 use crate::api::utils::response_status_json;
 
 use super::routers::handle;
-use super::typedef::Router;
+
+static ERROR_COLOR: &str = "\x1b[31m";
+static SUCCESS_COLOR: &str = "\x1b[32m";
+static RESET_COLOR: &str = "\x1b[0m";
 
 pub async fn srv(req: Request<Incoming>, address: SocketAddr) -> Result<Response<Full<Bytes>>, Infallible> {
     let path: Box<str> = req.uri().path().into();
@@ -21,8 +22,10 @@ pub async fn srv(req: Request<Incoming>, address: SocketAddr) -> Result<Response
             ok: false,
             error: err.get_msg()
         };
-        println!("-> Bad Return Error: {}, code: {}, path: {}, address: {}", err.get_msg(), err.get_status(), path, address);
+        println!("{ERROR_COLOR}-> Bad Return Error: {}, code: {}, path: {}, address: {}{RESET_COLOR}", err.get_msg(), err.get_status(), path, address);
         return Ok(response_status_json(value, *err.get_status()));
     }
+    println!("{SUCCESS_COLOR} Successful Connection: path: {}, address: {}{RESET_COLOR}", path, address);
+
     return Ok(res.unwrap());
 }
