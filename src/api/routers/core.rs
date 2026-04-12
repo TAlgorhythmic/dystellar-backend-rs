8d8061ac285a3d804d3af9426b7f76ba8cdad7e7
Algorhythmics <intivioli@gmail.com>
2026-04-12 20:43:45 +0200

    Update

--- src/api/routers/core.rs
+++ src/api/routers/core.rs
@@ -28,7 +28,6 @@ fn check_token(req: &Request<Incoming>) -> Result<(), BackendError> {
 
 fn privileged_middleware(req: &Request<Incoming>) -> Result<(), BackendError> {
     if req.headers().get("Target-Host").is_none() || ALLOWED_IP != req.headers().get("Target-Host").unwrap() {
-        println!("Not permitted");
         return Err(BackendError::new("Operation not permitted.", 401));
     }
     check_token(&req)?;
